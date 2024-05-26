9df33645b1de8463c68f7d3461049acc54bd6cdd
SergeiRyzhikov <sergeiryzhikovv@gmail.com>
2024-05-26 13:12:57 +0300

    add right PM1

--- main.cpp
+++ main.cpp
@@ -23,19 +23,22 @@ int PM1_embed(string path, string text) {
     for (int y = 0; y < rows; ++y) {
         for (int x = 0; x < cols; ++x) {
             if (rounds < binaryText.length()) {
-                char bit = binaryText[rounds];
+                int bit = binaryText[rounds]- '0';
                 cv::Vec3b pixel = image.at<cv::Vec3b>(y, x);
 
-                if (pixel[0]%2 != static_cast<int>(bit) ){
+                int blue_current_pixel = static_cast<int>(pixel[0]);
+
+                if (blue_current_pixel%2 != bit ){
                     int r = randomBinary();
+                    cout << blue_current_pixel << " " << bit << endl;
                     if (r==1){
-                        pixel[0] -=1;
+                        blue_current_pixel -=1;
                     }
                     else{
-                        pixel[0] +=1;
+                        blue_current_pixel +=1;
                     }
-                    
                 }
+                pixel[0] = blue_current_pixel;
                 image.at<cv::Vec3b>(y, x) = pixel;
             }
             else{
@@ -49,12 +52,41 @@ int PM1_embed(string path, string text) {
     return 0;
 }
 
+int PM1_extract(string path) {
+    
+    cv::Mat image = cv::imread(path);
+
+    if(image.empty()) {
+        cout << "Невозможно загрузить изображение. Проверьте путь к файлу." << endl;
+        return -1;
+    }
+    
+    // Получаем размеры изображения
+    int rows = image.rows;
+    int cols = image.cols;
+    string extractedText = "";
+    for (int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            cv::Vec3b pixel = image.at<cv::Vec3b>(y, x);
+
+            if (pixel[0]%2 == 0){
+                extractedText +="0";
+            }
+            else{
+                extractedText +="1";
+            }
+        }
+    }
+    string text = convertExtractText(extractedText);
+    cout << text <<endl;
+    return 0;
+}
 
 
 int main() {
 
-    LSB_embed("steg1.png", "my text");
-    LSB_extract("new_image.png");
+    PM1_embed("steg1.png", "my text");
+    PM1_extract("new_image.png");
 
     return 0;
 }
