01dc522e6368ef9606a90edfb8dc002095d25790
SergeiRyzhikov <sergeiryzhikovv@gmail.com>
2024-05-26 18:59:08 +0300

    main function

--- main.cpp
+++ main.cpp
@@ -12,10 +12,53 @@ using namespace std;
 
 
 int main() {
-    string text;
-    // cin >> text;
-    QIM_embed("steg1.png", "hi stegongrap", "new_image.png");
-    QIM_extract("new_image.png");
+    string method, choice, path, name, text;
+    cout << "Выберите стегонаграфический метод (PM1, QIM, LSB): " << endl;
+    cin >> method;
+    cout << "Выберите встраивание или извлечение: " << endl;
+    cin >> choice;
+    if (choice == "извлечение"){
+        cout << "Введите путь до изображения: " << endl;
+        cin >> path;
+        if (method == "QIM"){
+            QIM_extract(path);
+        }
+        else if (method == "LSB"){
+            LSB_extract(path);
+        }
+        else if (method == "PM1"){
+            PM1_extract(path);
+        }
+        else {
+            cout << "Метод введен неверно :(";
+        }
+    }
+    else if (choice == "встраивание"){
+        cout << "Введите путь до изображения: " << endl;
+        cin >> path;
+        cout << "Введите название стегоизображения: " << endl;
+        cin >> name;
+        cout << "Введите текст: " << endl;
+        cin >> text;
+        if (method == "QIM"){
+            QIM_embed(path, text, name);
+        }
+        else if (method == "LSB"){
+            LSB_embed(path, text, name);
+        }
+        else if (method == "PM1"){
+            PM1_embed(path, text, name);
+        }
+        else {
+            cout << "Метод введен неверно :(";
+        }
+    }
+    else {
+        cout << "Некорректно введенные данные :(";
+    }
+
+    // QIM_embed("steg1.png", "hi stegongrap", "new_image.png");
+    // QIM_extract("new_image.png");
 
     return 0;
 }
