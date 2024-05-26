91b5fdfc3efd4d7d1401ad580c9101a29ee4d1c8
SergeiRyzhikov <sergeiryzhikovv@gmail.com>
2024-05-26 12:22:20 +0300

    PM1 decomposition

--- main.cpp
+++ main.cpp
@@ -3,75 +3,8 @@
 #include <string>
 #include <vector>
 #include "utils.h"
+#include "PM1.h"
 using namespace std;
-
-
-
-int PM1_extract(string path) {
-
-    cv::Mat image = cv::imread(path);
-    if(image.empty()) {
-        cout << "Невозможно загрузить изображение. Проверьте путь к файлу." << endl;
-        return -1;
-    }
-    // Получаем размеры изображения
-    int rows = image.rows;
-    int cols = image.cols;
-    int rounds = 0;
-    string extractedText = "";
-    for (int y = 0; y < rows; ++y) {
-        for (int x = 0; x < cols; ++x) {
-            // Получаем текущий пиксель
-            cv::Vec3b pixel = image.at<cv::Vec3b>(y, x);
-
-            string binaryPixel = toBinary(static_cast<int>(pixel[0]));
-
-            char lastBit = binaryPixel[binaryPixel.length()-1];
-
-            extractedText += lastBit;
-        }
-    }
-    string text = convertExtractText(extractedText);
-
-    cout << text <<endl;
-    return 0;
-}
-
-int PM1_embed(string path, string text) {
-    string binaryText = convertText(text);
-    
-    cv::Mat image = cv::imread(path);
-
-    if(image.empty()) {
-        cout << "Невозможно загрузить изображение. Проверьте путь к файлу." << endl;
-        return -1;
-    }
-    // Получаем размеры изображения
-    int rows = image.rows;
-    int cols = image.cols;
-    int rounds = 0;
-    for (int y = 0; y < rows; ++y) {
-        for (int x = 0; x < cols; ++x) {
-            if (rounds < binaryText.length()) {
-                char bit = binaryText[rounds];
-                cv::Vec3b pixel = image.at<cv::Vec3b>(y, x);
-
-                string binaryPixel = toBinary(static_cast<int>(pixel[0]));
-                binaryPixel[binaryPixel.length()-1] = bit;
-                pixel[0] = binaryToDecimal(binaryPixel); 
-                
-                image.at<cv::Vec3b>(y, x) = pixel;
-            }
-            else{
-                break;
-            }
-            rounds +=1;
-        }
-    }
-    // Сохраняем измененное изображение
-    cv::imwrite("new_image.png", image);
-    return 0;
-}
 
 
 int main() {
@@ -81,3 +14,5 @@ int main() {
 
     return 0;
 }
+
+
