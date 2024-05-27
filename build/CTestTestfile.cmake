ee4580773b31a8c52e51a1faec71f86ba72f3cf3
SergeiRyzhikov <sergeiryzhikovv@gmail.com>
2024-05-27 12:50:13 +0300

    add docs in utils ctest command

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ryzhikov/source/project_steg
+# Build directory: /home/ryzhikov/source/project_steg/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTests "/home/ryzhikov/source/project_steg/build/MyTests")
+set_tests_properties(MyTests PROPERTIES  _BACKTRACE_TRIPLES "/home/ryzhikov/source/project_steg/CMakeLists.txt;23;add_test;/home/ryzhikov/source/project_steg/CMakeLists.txt;0;")
--- utils.cpp
+++ utils.cpp
@@ -5,12 +5,21 @@
 #include <stdexcept>
 
 using namespace std;
-
+/**
+ * @brief Generates a random binary digit (0 or 1).
+ * 
+ * @return int Random binary digit (0 or 1).
+ */
 int randomBinary() {
     return rand() % 2; 
 }
 
-
+/**
+ * @brief Adds leading zeros to a string to make its length 8.
+ * 
+ * @param s The input string.
+ * @return std::string The input string with leading zeros added to make its length 8.
+ */
 string addZeros(string s) {
     if (s.length()<8) {
         s.insert(0, 8-s.length(), '0');
@@ -18,6 +27,13 @@ string addZeros(string s) {
     return s;
 }
 
+/**
+ * @brief Converts a decimal number to its binary representation as a string.
+ * 
+ * @param decimal The decimal number to be converted.
+ * @return std::string The binary representation of the decimal number.
+ * @throw std::invalid_argument If the decimal number is negative.
+ */
 string toBinary(int decimal) {
     if (decimal == 0) return "0";
     if (decimal < 0) throw invalid_argument("Negative numbers are not supported");
@@ -32,6 +48,13 @@ string toBinary(int decimal) {
     return binary;
 }
 
+/**
+ * @brief Converts a text string to its binary representation.
+ * 
+ * @param text The text string to be converted.
+ * @return std::string The binary representation of the text string.
+ * @throw std::invalid_argument If the text contains unsupported characters.
+ */
 string convertText(string text){
     string data;
     for (int i = 0; i < text.length(); ++i) {
@@ -49,6 +72,13 @@ string convertText(string text){
     return data;
 }
 
+/**
+ * @brief Converts a binary string to its decimal representation.
+ * 
+ * @param binary The binary string to be converted.
+ * @return int The decimal representation of the binary string.
+ * @throw std::invalid_argument If the binary string contains characters other than '0' or '1'.
+ */
 int binaryToDecimal(const std::string& binary) {
     int decimal = 0;
     int power = 1; // Степень двойки, которая будет умножаться на каждый бит
@@ -69,7 +99,14 @@ int binaryToDecimal(const std::string& binary) {
     return decimal;
 }
 
+/**
+ * @brief Converts a binary string representing text back to its original text form.
+ * 
+ * @param extractedText The binary string representing the text.
+ * @return std::string The original text.
+ */
 string convertExtractText(string extractedText){
+
     string text;
     for (int i = 0; i<extractedText.length(); i+=8){
         int code = binaryToDecimal(extractedText.substr(i, 8));
